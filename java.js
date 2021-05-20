4cf1b62a6324b007e7a008f45e7b429d842893e0
jeosafaalves <80703463+jeosafaalves@users.noreply.github.com>
2021-05-20 17:09:37 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,4 @@
+const nome =prompt ("Qual Seu Nome Completo?");
+const ano =prompt (" Em Que Ano Você Nasceu?");
+const anodenascimento= 2021-ano;
+alert("seu nome é"  +nome + "e sua idade" +anodenascimento+"anos");
